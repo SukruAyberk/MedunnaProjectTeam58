5f1ae50d31ddc7f4feb0694230a2511593c71c0b
ElvinaOzturk <101992187+ElvinaOzturk@users.noreply.github.com>
2022-08-11 14:05:52 +0300

    elvina

--- src/test/java/pages/ElvinaPage.java
+++ src/test/java/pages/ElvinaPage.java
@@ -1,5 +1,30 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class ElvinaPage{
+    public ElvinaPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//a[@class='dropdown-item active']")
+    public WebElement settingButonu;
+
+    @FindBy(xpath = "//input[@id='firstName']")
+    public WebElement firstnameGuncelleme;
+
+    @FindBy(xpath = "//input[@id='lastName']")
+    public WebElement lastnameGuncelleme;
+
+    @FindBy(xpath = "//input[@id='email']")
+    public WebElement eMailGuncelleme;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement saveButonu;
+
+
 
 }
